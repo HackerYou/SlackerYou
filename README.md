87ac8a3176e8f555c4c953ee3567136127ada1dc
wesbos <wesbos@gmail.com>
2015-07-21 09:40:39 -0400

    🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SlackerYou
-A simple, real time chatroom built with Firebase and jQuery.
+
+A simple, real time chatroom built with Firebase and jQuery. Come learn to code with us at [HackerYou](http://hackeryou.com) 
+
+## Try the demo
+
+[![](http://f.cl.ly/items/1b2E1X323X3D312e2v2g/ss%202015-07-21%20at%209.36.42%20AM.png)](http://hackeryou.github.com)
---
+++ scripts.js
@@ -0,0 +1,48 @@
+  // Create a referece to firebase
+  var messagesRef = new Firebase('https://hackeryou.firebaseio.com/chat');
+
+  // C.R.E.A.M -  cache your elements
+  var messageField = $('#messageInput');
+  var nameField = $('#nameInput');
+  var messageList = $('.messages');
+
+  // Listen for the form submit
+  $('.chat').on('submit',function(e) {
+
+    // stop the form from submitting
+    e.preventDefault();
+
+    // create a message object
+    var message = {
+      name : nameField.val(),
+      text : messageField.val()
+    }
+
+    // Save Data to firebase
+    messagesRef.push(message);
+    
+    // clear message field
+    messageField.val('');
+
+  });
+
+  // Add a callback that is triggered for each chat message
+  // this is kind of like an Ajax request, but they come in via websockets
+  // 10 of them will load on page load, and any future messages will as well
+  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+    
+    // Get data from returned 
+    var data = snapshot.val();
+    var username = data.name || 'anonymous';
+    var message = data.text;
+
+    // Create an element
+    var nameElement = $('<strong>').text(username);
+    var messageElement = $('<li>').text(message).prepend(nameElement);
+    
+    // Add the message to the DOM
+    messageList.append(messageElement);
+
+    // Scroll to the bottom of the message list
+    messageList[0].scrollTop = messageList[0].scrollHeight;
+  });
