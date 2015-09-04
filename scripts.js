2586507be111109525ae9b82a2164d918abdbfcf
David Luecke <daff@neyeon.com>
2015-09-03 18:22:29 -0700

    Some style changes and refactoring for use in a tutorial

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
-  // Create a referece to firebase
+  // Create a reference to firebase
   var messagesRef = new Firebase('https://hackeryou.firebaseio.com/chat');
 
   // C.R.E.A.M -  cache your elements
   var messageField = $('#messageInput');
   var nameField = $('#nameInput');
   var messageList = $('.messages');
+
+  function addMessage(data) {
+    var username = data.name || 'anonymous';
+    var message = data.text;
+
+    // Create an element
+    var nameElement = $('<strong>').text(username);
+    var messageElement = $('<li>').text(message).prepend(nameElement);
+
+    // Add the message to the DOM
+    messageList.append(messageElement);
+
+    // Scroll to the bottom of the message list
+    messageList[0].scrollTop = messageList[0].scrollHeight;
+  }
 
   // Listen for the form submit
   $('.chat').on('submit',function(e) {
@@ -20,7 +35,7 @@
 
     // Save Data to firebase
     messagesRef.push(message);
-    
+
     // clear message field
     messageField.val('');
 
@@ -30,19 +45,6 @@
   // this is kind of like an Ajax request, but they come in via websockets
   // 10 of them will load on page load, and any future messages will as well
   messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-    
-    // Get data from returned 
-    var data = snapshot.val();
-    var username = data.name || 'anonymous';
-    var message = data.text;
-
-    // Create an element
-    var nameElement = $('<strong>').text(username);
-    var messageElement = $('<li>').text(message).prepend(nameElement);
-    
-    // Add the message to the DOM
-    messageList.append(messageElement);
-
-    // Scroll to the bottom of the message list
-    messageList[0].scrollTop = messageList[0].scrollHeight;
+    // Get data from returned
+    addMessage(snapshot.val());
   });
