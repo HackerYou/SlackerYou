cadf1238b8653a2dd6d4fcc5fa777eb4a3244738
David Luecke <daff@neyeon.com>
2015-09-04 17:11:37 -0700

    A jQuery + Feathers real-time chat demo.

--- scripts.js
+++ scripts.js
@@ -1,50 +1,56 @@
-  // Create a reference to firebase
-  var messagesRef = new Firebase('https://hackeryou.firebaseio.com/chat');
+// Create a Socket.io connection
+var socket = io();
+// Create a Feathers client that uses that connection
+var app = feathers().configure(feathers.socketio(socket));
+// Get the messages service
+var messages = app.service('messages');
 
-  // C.R.E.A.M -  cache your elements
-  var messageField = $('#messageInput');
-  var nameField = $('#nameInput');
-  var messageList = $('.messages');
+// C.R.E.A.M -  cache your elements
+var messageField = $('#messageInput');
+var nameField = $('#nameInput');
+var messageList = $('.messages');
 
-  function addMessage(data) {
-    var username = data.name || 'anonymous';
-    var message = data.text;
+function addMessage(data) {
+  var username = data.name || 'anonymous';
+  var message = data.text;
 
-    // Create an element
-    var nameElement = $('<strong>').text(username);
-    var messageElement = $('<li>').text(message).prepend(nameElement);
+  // Create an element
+  var nameElement = $('<strong>').text(username);
+  var messageElement = $('<li>').text(message).prepend(nameElement);
 
-    // Add the message to the DOM
-    messageList.append(messageElement);
+  // Add the message to the DOM
+  messageList.append(messageElement);
 
-    // Scroll to the bottom of the message list
-    messageList[0].scrollTop = messageList[0].scrollHeight;
+  // Scroll to the bottom of the message list
+  messageList[0].scrollTop = messageList[0].scrollHeight;
+}
+
+// Listen for the form submit
+$('.chat').on('submit',function(e) {
+
+  // stop the form from submitting
+  e.preventDefault();
+
+  // create a message object
+  var message = {
+    name : nameField.val(),
+    text : messageField.val()
   }
 
-  // Listen for the form submit
-  $('.chat').on('submit',function(e) {
+  // Save data to the messages service
+  messages.create(message);
 
-    // stop the form from submitting
-    e.preventDefault();
+  // clear message field
+  messageField.val('');
 
-    // create a message object
-    var message = {
-      name : nameField.val(),
-      text : messageField.val()
-    }
+});
 
-    // Save Data to firebase
-    messagesRef.push(message);
+// Add new messages
+messages.on('created', addMessage);
 
-    // clear message field
-    messageField.val('');
-
-  });
-
-  // Add a callback that is triggered for each chat message
-  // this is kind of like an Ajax request, but they come in via websockets
-  // 10 of them will load on page load, and any future messages will as well
-  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-    // Get data from returned
-    addMessage(snapshot.val());
-  });
+// Find the last 10 messages and add them
+messages.find(function(error, messages) {
+  if(messages) {
+    messages.forEach(addMessage);
+  }
+});
